419b0b6715864b1f98d19228ddb6880520ad9e74
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-12-08 19:55:58 +0500

    made a basic player controller and some platforms

---
+++ Time-Warp/Assets/Scripts/CrumblingPlatform.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class CrumblingPlatform : MonoBehaviour
+{
+    [SerializeField] float crumbleDelay = 3f;
+    [SerializeField] string playerTag = "Player";
+
+    Collider2D platformCollider;
+    SpriteRenderer platformRenderer;
+    bool crumbleStarted;
+
+    void Awake()
+    {
+        platformCollider = GetComponent<Collider2D>();
+        platformRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (crumbleStarted) return;
+        if (!collision.collider.CompareTag(playerTag)) return;
+
+        crumbleStarted = true;
+        StartCoroutine(CrumbleAfterDelay());
+    }
+
+    IEnumerator CrumbleAfterDelay()
+    {
+        yield return new WaitForSeconds(crumbleDelay);
+
+        if (platformCollider != null) platformCollider.enabled = false;
+        if (platformRenderer != null) platformRenderer.enabled = false;
+    }
+}
--- Time-Warp/Assets/Scripts/PlayerController.cs
+++ Time-Warp/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float acceleration = 15f;
+
+    [Header("Jump")]
+    [SerializeField] float jumpForce = 12f;
+    [SerializeField] LayerMask groundLayer;
+
+    Rigidbody2D rb;
+    Collider2D col;
+    float horizontalInput;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (col.IsTouchingLayers(groundLayer) && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float targetSpeed = horizontalInput * moveSpeed;
+        float speedDifference = targetSpeed - rb.linearVelocity.x;
+        float movement = Mathf.Clamp(speedDifference, -acceleration * Time.fixedDeltaTime, acceleration * Time.fixedDeltaTime);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x + movement, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.y = jumpForce;
+        rb.linearVelocity = velocity;
     }
 }
