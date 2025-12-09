c4bb9b39173526d86a94730721896b3279c43f33
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-12-09 20:28:12 +0500

    Implemented walking animations + character flipping

--- Time-Warp/Assets/Scripts/PlayerController.cs
+++ Time-Warp/Assets/Scripts/PlayerController.cs
@@ -9,39 +9,76 @@ public class PlayerController : MonoBehaviour
     [Header("Jump")]
     [SerializeField] float jumpForce = 12f;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] float groundCheckDistance = 0.05f;
 
     Rigidbody2D rb;
+    Animator animator;
     Collider2D col;
+
     float horizontalInput;
+    bool isGrounded;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         col = GetComponent<Collider2D>();
     }
 
     void Update()
     {
+        // Read input
         horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        if (col.IsTouchingLayers(groundLayer) && Input.GetButtonDown("Jump"))
+        // Ground Check
+        isGrounded = Physics2D.BoxCast(
+            col.bounds.center,
+            col.bounds.size,
+            0f,
+            Vector2.down,
+            groundCheckDistance,
+            groundLayer
+        );
+
+        // Jump
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             Jump();
+        }
+
+        // --- Animation ---
+        animator.SetBool("IsWalking", Mathf.Abs(horizontalInput) > 0.1f);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("VerticalVelocity", rb.linearVelocity.y);
+
+        // --- Flip Sprite ---
+        if (horizontalInput > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontalInput < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 
     void FixedUpdate()
     {
+        // Smooth horizontal acceleration
         float targetSpeed = horizontalInput * moveSpeed;
         float speedDifference = targetSpeed - rb.linearVelocity.x;
-        float movement = Mathf.Clamp(speedDifference, -acceleration * Time.fixedDeltaTime, acceleration * Time.fixedDeltaTime);
+
+        float movement = Mathf.Clamp(
+            speedDifference,
+            -acceleration * Time.fixedDeltaTime,
+            acceleration * Time.fixedDeltaTime
+        );
+
         rb.linearVelocity = new Vector2(rb.linearVelocity.x + movement, rb.linearVelocity.y);
     }
 
     void Jump()
     {
-        Vector2 velocity = rb.linearVelocity;
-        velocity.y = jumpForce;
-        rb.linearVelocity = velocity;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 }
