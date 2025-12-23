5b982d7d358e55eedd661201781f93290bc2f7c1
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-12-23 11:41:49 +0500

    Made start scene + improved buttons + added click sfx

---
+++ Time-Warp/Assets/Scripts/MainButtons.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainButtons : MonoBehaviour
+{
+    public AudioClip clickClip;
+    public void LoadLevel1()
+    {
+        AudioManager.Instance.PlaySFX(clickClip, 0.7f);
+        SceneManager.LoadScene("Level-1");
+    }
+
+    public void LoadCredits()
+    {
+        AudioManager.Instance.PlaySFX(clickClip, 0.7f);
+        SceneManager.LoadScene("Credits");
+    }
+}
